c608a304862e1aa72d07334ff7165212312aa6ea
keiichirokimoto <kei199651@gmail.com>
2021-09-21 21:10:26 +0900

    投稿ページ＆登録処理

--- src/app/Http/Controllers/HobbyController.php
+++ src/app/Http/Controllers/HobbyController.php
@@ -5,9 +5,73 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Models\Hobby;
+use App\Models\Post;
+use DB;
+use Session;
+use Form;
+
 
 class HobbyController extends Controller
 {
+    // 一覧ページ
+    public function list(Request $request){
+        return view('hobbys.hobbys_list');
+    }
+
+    // 趣味投稿ページ
+    public function regist(Request $request){
+        $nullitem = [ '' => '選択して下さい' ];
+        //フォームの構築
+        $form = [
+            'category'   => Form::select('category', $nullitem + __('define.category'), '',["class"=>"", "id"=>"category"] ),
+            'prefecture' => Form::select('prefecture', $nullitem + __('define.prefecture'), '',["class"=>"", "id"=>"prefecture"] ),
+        ];
+        return view('hobbys.regist')->with('form',$form);
+    }
+
+    // 趣味投稿処理
+    public function store(Request $request){
+        $request->validate([
+            'category'              =>'required',
+            'title'                 =>'required|max:30',
+            'text'                  =>'required|max:300',
+            'hobby_img_path'        =>'image',
+            'prefecture'            =>'required',
+            'municipalities'        =>'max:10'
+        ],
+        [
+            'required'              =>'必須入力です。',
+            'image'                 =>'画像はjpg、png、bmp、gif、svg、webpのファイルを選択して下さい。'
+        ]);
+
+        if ($file = $request->hobby_img_path) {
+            // getClientOriginalName()  拡張子を含め、アップロードしたファイルのファイル名を取得することができる。
+            $fileName = time() . $file->getClientOriginalName();
+            //public_path() publicディレクトリの完全パスを返す。publicディレクトリ内にuploadsディレクトリを作成。
+            $target_path = public_path('uploads/post/');
+            $file->move($target_path, $fileName);
+        }else {
+            $fileName = "";
+        }
+
+        // ログインしているuser_idの取得
+        // $user = auth::user()->id;
+
+        $post = new Post;
+        // ログインしているuser_idを何処で拾うか
+        // $post->user_id     = $user;
+        $post->category       = $request->category;
+        $post->title          = $request->title;
+        $post->text           = $request->text;
+        $post->hobby_img_path = $fileName;
+        $post->prefecture     = $request->prefecture;
+        $post->municipalities = $request->municipalities;
+        $post->save();
+
+        // $message = "新規登録が完了しました。";
+        return redirect()->route('index');
+    }
+
     public function edit(Request $request)
     {
         $user = User::find($request->id);
--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HobbyController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,6 +20,15 @@ Route::get('/', function () {
 
 Auth::routes();
 
+// 投稿一覧ページ
+Route::any('/index',        [HobbyController::class, 'list'])->name('hobby.list');
+
+// 趣味新規投稿一覧ページ
+Route::any('/hobby/regist', [HobbyController::class, 'regist'])->name('hobby.regist');
+// 趣味登録処理
+Route::post('/regist',      [HobbyController::class, 'store'])->name('hobby.proc');
+
 Route::get('/user/edit/{id}', [App\Http\Controllers\HobbyController::class, 'edit'])->name('user_edit');
 Route::post('/user/edit/{id}', [App\Http\Controllers\HobbyController::class, 'update'])->name('user_update');
+
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
